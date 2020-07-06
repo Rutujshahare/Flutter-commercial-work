649085ff4b182905cb074832a5a93f6b18f7b8a5
Rutujshahare <51992103+Rutujshahare@users.noreply.github.com>
2020-07-06 10:57:23 +0530

    my First App in flutter on my own data Display added

---
+++ RutujDataApp/android/app/src/main/kotlin/com/example/RutujDataApp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.RutujDataApp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- ScaffoldApp/lib/main.dart
+++ ScaffoldApp/lib/main.dart
@@ -1,11 +1,61 @@
 import 'package:flutter/material.dart';
 
-void main()=>runApp(MaterialApp(
-  home:Scaffold(
-    appBar:AppBar(
-     title:Text('Scaffolding widget tutorial App'),
-     centerTitle:true,
-    ),
-  ),
+void main() =>runApp(MaterialApp(
+home:Home(),
 ));
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        title:Text('GorakshanApp'),
+        centerTitle:true,
+      ),
+     body:Column(
+       crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:<Widget>[
+            Container(child: Image.asset('assets/images/logo.jpg'),
+            width:103,
+            ),
+          ]
+        ),
+        Container(
+          color:Colors.red[400],
+          padding:EdgeInsets.all(50),
+          child:Center(child: Text('Do your duty that is your Beauty'),
+          ),
+          
+        ),
+          Container(
+          color:Colors.yellow[300],
+          padding:EdgeInsets.all(50),
+          child: Image.asset('assets/images/LogoDesign.jpg'),
+          width:230,
+        ),
+          Container(
+          color:Colors.pink[400],
+          padding:EdgeInsets.all(50),
+          child: RaisedButton.icon(
+            onPressed:() {},
+            icon:Icon(Icons.add_comment),
+            label:Text('add the comment'),
+
+          ),
+        ),
+      ],
+     ),
+      floatingActionButton: FloatingActionButton(
+        child:Text('next'),
+        backgroundColor: Colors.green[400],
+        
+      ),
+      
+    );
+  }
+}
 
